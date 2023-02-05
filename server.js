74d370e8453fad80007071aaecf05a391c4c0191
sayan pramanick <sayanp05@gmail.com>
2023-02-05 23:34:47 +0530

    Code Changes Server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/sms-app'));
+app.use(express.static(__dirname + '/sms-app'));
 app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/sms-app/index.html'));});
+{res.sendFile(path.join(__dirname+'/sms-app/index.html'));});
 
 app.listen(process.env.PORT || 8080);
